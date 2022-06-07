7cc16f1a909619fb797e3fb70c91404caeb5ac22
Leninvc13 <leninvalencia@hotmail.com>
2022-06-06 19:50:25 -0500

    Mejoras 1

---
+++ template-string.js
@@ -0,0 +1,14 @@
+const nombre = 'Deadpool'
+const real = 'Wade Winston'
+
+const complete = nombre + real;
+
+const template = `${ nombre } ${ real }`;
+
+console.log(template)
+
+
+const html = `
+<h1> Hola<h1>
+`
+console.log(html)
